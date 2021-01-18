82ceda77c0a57f63447c28db8f72cb51bfb8447d
Adrien Martin <adrien.v.martin@gmail.com>
2021-01-17 19:02:01 -0600

    Replace window content with markerContent

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -32,7 +32,7 @@ export class CustomMap {
 
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: "Hi there"
+        content: markerType.markerContent()
       });
 
       infoWindow.open(this.googleMap, marker);
