bfb12e3e76dbe8353d31e8afc039f6b193376c88
Adrien Martin <adrien.v.martin@gmail.com>
2021-01-17 18:53:04 -0600

    Add popup window functionality

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -21,12 +21,20 @@ export class CustomMap {
   }
 
   addMarker(markerType: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: markerType.location.lat,
         lng: markerType.location.lng
       }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi there"
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
